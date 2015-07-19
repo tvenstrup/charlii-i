0e32abf46853d51eb0b7a8cf9fbf80d515cebcde
Rasmus Hagedorn Tvenstrup <tvenstrup@gmail.com>
2015-07-19 10:36:17 +0200

    Removed unused namespaces

--- charlii-i/Controllers/AccountController.cs
+++ charlii-i/Controllers/AccountController.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using charlii_i.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using charlii_i.Models;
 
 namespace charlii_i.Controllers
 {
--- charlii-i/Controllers/ManageController.cs
+++ charlii-i/Controllers/ManageController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using charlii_i.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using charlii_i.Models;
 
 namespace charlii_i.Controllers
 {
