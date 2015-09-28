e5add0db6cd247e074eecab3409d60a967ccef68
Rasmus Hagedorn Tvenstrup <tvenstrup@gmail.com>
2015-09-28 21:47:53 +0200

    Created Menu.css and Page.css

--- charlii-i/App_Start/BundleConfig.cs
+++ charlii-i/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace charlii_i
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/menu.css",
+                      "~/Content/page.css"));
         }
     }
 }
